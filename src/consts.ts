c35de1488d1e7bcc864075b3bccd000226cbe1fa
Eyal Alfasi <eyalalf@gmail.com>
2024-12-17 21:43:56 +0200

    Update homepage description

--- src/consts.ts
+++ src/consts.ts
@@ -10,7 +10,7 @@ export const SITE: Site = {
 
 export const HOME: Metadata = {
   TITLE: 'Home',
-  DESCRIPTION: 'Astro Nano is a minimal and lightweight blog and portfolio.',
+  DESCRIPTION: "This is Eyal Alfasi's personal website.",
 };
 
 export const BLOG: Metadata = {
